e96ee07fca74353ad6a038b432599e019f8909c8
ghqnfgh <ghqnfgh@gmail.com>
2020-12-24 13:42:45 +0900

    Init repository

---
+++ Datatype.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProgramming
+{
+    class Datatype
+    {
+        static void Main(string[] args)
+        {
+            // 기본 출력 방식
+            // System : namespace. root namespace는 global이며 System namespace는 원래 global::System임.
+            // System.Console : .NET Framework 클래스
+            System.Console.WriteLine("Hello World...");
+            
+        }
+    }
+}
