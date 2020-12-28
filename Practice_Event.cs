d871e0a9808e8bfdb29517be3ef5fcb0a54a5d1c
ghqnfgh <ghqnfgh@gmail.com>
2020-12-28 15:29:27 +0900

    Event in CSharp Partially

---
+++ Practice_Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProgramming
+{
+    class Practice_Event
+    {
+        public string text;
+        public event EventHandler Click;
+
+        public void MouseButtonDown()
+        {
+            if(this.Click != null)
+            {
+                Click(this, EventArgs.Empty);
+            }
+        }
+
+    }
+}
---
+++ Use_method2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProgramming
+{
+    class Use_method2
+    {
+        /*void btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 클릭");
+        }
+        static void Main(string[] args)
+        {
+            Practice_Event PEtmp = new Practice_Event();
+            // 이벤트가 발생한 경우 진행할 메서드를 파라미터로 입력.
+            // => btn_Click : Event handler 메서드. 
+            PEtmp.Click += new EventHandler(btn_Click);
+            // +=로 이벤트에 가입시킬 수 있으며, -=로 이벤트에서 탈퇴 시킬 수 있다.
+            // 위 기능을 이용하여 내부 메서드인 MouseButtonDown()은 이벤트 핸들러 클래스인
+            // Click을 기준으로 null인지 아닌지에 따라 이벤트 가입자가 있는지 여부를 확인한다.
+
+            // EventHandler 클래스는 생성자로 (object sender, EventArgs a )를 갖고 있다.
+            // sender는 이벤트 핸들러의 이름이며, EventArgs는 이벤트 핸들러에 전달하는 변수 목록이다.
+
+            // Q) 위의 프로그램은 c# 기반의 GUI 프로그램을 작성할 때 주로 사용되는 것으로 보이는데
+            // 이 때는 어떤 구조로 프로그램을 생성해야 하는가?
+        }*/
+        static void Main()
+        {
+
+        }
+
+    }
+}
