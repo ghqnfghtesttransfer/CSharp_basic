9735ac2537d6491ab095edcfd87ccb116b22ce51
ghqnfgh <ghqnfgh@gmail.com>
2020-12-28 10:20:27 +0900

    For Condition in CSharp, got error in second method

---
+++ Practice_Condition.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProgramming
+{
+    class Practice_Condition
+    {
+        static bool v = false;
+        static bool c = false;
+        static bool l = false;
+        public void Method()
+        {
+            string args = Console.ReadLine();
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage : App.exe options");
+                return;
+            }
+            string option = args;
+
+            switch (option.ToLower())
+            {
+                case "/v":
+                    v = true;
+                    Console.WriteLine("V True");
+                    break;
+                case "/c":
+                    c = true;
+                    Console.WriteLine("C True");
+                    break;
+                case "/l":
+                    l = true;
+                    Console.WriteLine("l True");
+                    break;
+                default:
+                    Console.WriteLine("Unknown argument {0}", option);
+                    break;
+            }
+
+            /*
+             * Q) Method에 string args[]를 넘기는 방식으로 Console Data를 읽어오려
+             * 사이트에 기재된 방식을 따라해보았으나 알 수 없는 문제가 발생함.
+             * */
+            /*
+             static void Main(string[] args)
+            {            
+                if (args.Length != 1)
+                {
+                    Console.WriteLine("Usage: MyApp.exe option");
+                    return;
+                }
+
+                string option = args[0];
+                switch (option.ToLower())
+                {
+                    case "/v":
+                    case "/verbose":
+                        verbose = true;                    
+                        break;
+                    case "/c":
+                        continueOnError = true;
+                        break;
+                    case "/l":
+                        logging = true;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown argument: {0}", option);
+                        break;
+                }
+            }
+             */
+        }
+    }
+}
--- Use_method.cs
+++ Use_method.cs
@@ -8,7 +8,7 @@ namespace CSharpProgramming
 {
     class Use_method
     {
-        static void Main()
+        static void Main(string[] args)
         {
             // 기본 출력 방식
             // System : namespace. root namespace는 global이며 System namespace는 원래 global::System임.
@@ -44,11 +44,19 @@ namespace CSharpProgramming
             // Practice_Enum PEtmp = new Practice_Enum();
             // PEtmp.Method();
 
+            // 6) Condition
+            // Practice_Condition PCtmp = new Practice_Condition();
+            // PCtmp.Method();
+
+            // 7) 
+
+
             // Console 자동 종료 방지
             // Console.ReadLine() : 엔터 키 입력 시 콘솔 종료
             Console.ReadKey(); //  아무 키 입력 시 콘솔 종료,
             // 실질적으로는 키보드 입력을 받는 역할이지만 키입력을 받고 추가 행동을 진행하지 않아
             // 키 입력 후에 콘솔을 종료하는 행동인 것 처럼 보이는 것.
+
         }
     }
 }
