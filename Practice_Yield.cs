33f620aa538967952f41d4dd4e216da46c4ad948
ghqnfgh <ghqnfgh@gmail.com>
2020-12-28 11:38:23 +0900

    do later, when read Advanced Programming

---
+++ Practice_Yield.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProgramming
+{
+    class Practice_Yield
+    {
+        // Yield 구문은 고급으로 사용되는 문법이라 기재되어 있음.
+        // IEnumerator( Iterator ) 인터페이스를 사용하는 함수를 구현하여
+        // 데이터 처리를 진행 시에 부분별로 나누어 필요에 따라 사용한다 함. 일단은 패스
+    }
+}
